add0b00333c03ae976df419ee4a0bc858ec73c43
Quinnterruption <83046743+Quinnterruption@users.noreply.github.com>
2025-12-02 14:46:11 -0600

    Added Comments
    Added comments in some header files

--- extra/playback.h
+++ extra/playback.h
@@ -14,20 +14,58 @@
 
 
 class Playback {
-    uint64_t startTime;
-    uint64_t endTime;
+    uint64_t startTime{};
+    uint64_t endTime{};
     std::ofstream outFile;
     WireFrame prevWireFrame;
 public:
+    /**
+     * Default constructor initializes all variables to 0
+     */
     Playback() = default;
 
+    /**
+     * @return the current time in milliseconds since the Linux Epoch
+     */
     static uint64_t currentTimeMillis();
 
+    /**
+     * Creates a file with the name "MM-DD-YYYY_HH-MM-SS"
+     * Sets startTime and endTime to their respective values
+     * @param duration the time to record for
+     */
     void startRecord(int duration);
+
+    /**
+     * Creates a file with the chosen name
+     * Sets startTime and endTime to their respective values
+     * @param name the name of the file
+     * @param duration the time to record for
+     */
     void startRecord(const std::string &name, int duration);
+
+    /**
+     * Closes outFile if it is open
+     */
     void endRecord();
+
+    /**
+     * @return true if outFile is open
+     */
     bool recording();
+
+    /**
+     * Writes any changes in the given WireFrame to outFile
+     * @param wireFrame the WireFrame to write to a file
+     */
     void update(WireFrame wireFrame);
+
+    /**
+     * Replays all movements from a ".rndr" file
+     * @param hwnd the current window
+     * @param windowBuffer the current window's WindowBuffer
+     * @param filePath the path of the file to read
+     */
     static void replay(HWND__ *hwnd, WindowBuffer &windowBuffer, const std::string& filePath);
 };
 
--- extra/wireFrame.h
+++ extra/wireFrame.h
@@ -12,11 +12,22 @@
 
 using std::array;
 
+/**
+ * A wrapper struct for std::array<double, 3>
+ * Designed to store coordinates
+ */
 struct coord {
     array<double, 3> coordinates{};
 
+    /**
+     * Default constructor initializes coordinates to {0, 0, 0}
+     */
     coord() = default;
 
+    /**
+     * Constructor for coord that takes a double array with 3 values
+     * @param init the initial elements for the double array
+     */
     coord(const std::initializer_list<double> init) {
         if (init.size() != 3) {
             throw std::invalid_argument("Coord must have exactly 3 elements");
@@ -24,11 +35,11 @@ struct coord {
         std::copy(init.begin(), init.end(), coordinates.begin());
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const coord& obj) {
-        os << obj[0] << ',' << obj[1] << ',' << obj[2];
-        return os;
-    }
-
+    /**
+     * Converts a string to a coord
+     * @param str a string formatted as "int,int,int" to be parsed
+     * @return a coord parsed from the string as {int, int, int}
+     */
     static coord stoc(std::string str) {
         double x = std::stoi(str.substr(0, str.find(',')));
         str = str.substr(str.find(',') + 1);
@@ -38,6 +49,11 @@ struct coord {
         return {x, y, z};
     }
 
+    /**
+     * Adds two coords and returns a coord
+     * @param obj the coord to add
+     * @return the resulting coord
+     */
     coord operator+(const coord& obj) const {
         coord temp = {};
         for (int i = 0; i < 3; i++) {
@@ -46,6 +62,11 @@ struct coord {
         return temp;
     }
 
+    /**
+     * Subtracts two coords and returns a ooord
+     * @param obj the coord to subtract
+     * @return the resulting coord
+     */
     coord operator-(const coord& obj) const {
         coord temp = {};
         for (int i = 0; i < 3; i++) {
@@ -54,13 +75,11 @@ struct coord {
         return temp;
     }
 
-    coord& operator-=(const coord& obj) {
-        for (int i = 0; i < 3; i++) {
-            coordinates[i] -= obj[i];
-        }
-        return *this;
-    }
-
+    /**
+     * Adds a coord and assigns it to the current coord
+     * @param obj the coord to add
+     * @return the resulting coord
+     */
     coord& operator+=(const coord& obj) {
         for (int i = 0; i < 3; i++) {
             coordinates[i] += obj[i];
@@ -68,27 +87,80 @@ struct coord {
         return *this;
     }
 
+    /**
+     * Subtracts a coord and assigns it to the current coord
+     * @param obj the coord to subtract
+     * @return the resulting coord
+     */
+    coord& operator-=(const coord& obj) {
+        for (int i = 0; i < 3; i++) {
+            coordinates[i] -= obj[i];
+        }
+        return *this;
+    }
+
+    /**
+     * Formats and writes a coordinate to an ostream
+     * @param os the stream to write to
+     * @param obj the coord to write to the stream
+     * @return the stream
+     */
+    friend std::ostream& operator<<(std::ostream& os, const coord& obj) {
+        os << obj[0] << ',' << obj[1] << ',' << obj[2];
+        return os;
+    }
+
+    /**
+     * Checks equivalency of two coords
+     * @param obj the coord to compare
+     * @return true if they are equal
+     */
     bool operator==(const coord& obj) const {
         return coordinates == obj.coordinates;
     }
 
+    /**
+     * Gets the element at a certain position
+     * @param idx the index of the coord
+     * @return the element at idx
+     */
     double& operator[](const std::size_t idx) {
         return coordinates[idx];
     }
 
+    /**
+     * Gets the constant element at a certain position
+     * @param idx the index of the coord
+     * @return the constant element at idx
+     */
     const double& operator[](const std::size_t idx) const {
         return coordinates[idx];
     }
 };
 
+/**
+ * Rotation Flags used to check which axes to rotate over
+ */
 enum rotationFlags {
     rotateX = 1,
     rotateY = 2,
     rotateZ = 4
 };
 
+/**
+ * A class to store and edit the WireFrame data
+ */
 class WireFrame {
+    /**
+     * @return the origin of the WireFrame
+     */
     coord getOrigin();
+
+    /**
+     * @param first the first 3D matrix to multiply
+     * @param second the second 3D matrix to multiply
+     * @return the resulting 3D matrix
+     */
     static matrix3 matrixMult(const matrix3& first, const matrix3& second);
 
     int rotateFlags = 0;
@@ -99,8 +171,25 @@ public:
     coord midPoint;
     array<coord, 8> coordinates;
 
+    /**
+     * Default constructor initializes all variables to 0
+     */
     WireFrame() = default;
+
+    /**
+     * Creates a WireFrame with coordinates according to init
+     * Will be deprecated later
+     * @param init the coordinates of every corner
+     */
     WireFrame(std::initializer_list<coord> init);
+
+    /**
+     * Creates a WireFrame according to User specifications
+     * @param topLeft the top left coordinate of the WireFrame
+     * @param height the height of the WireFrame
+     * @param length the length of the WireFrame
+     * @param depth the depth of the WireFrame
+     */
     WireFrame(const coord &topLeft, double height, double length, double depth);
 
     /* DEPRECATED
@@ -109,11 +198,39 @@ public:
         return os;
     }*/
 
+    /**
+     * Checks if 2 WireFrames are equal
+     * @param obj the WireFrame to compare
+     * @return true if the WireFrames are equal
+     */
     bool operator==(const WireFrame& obj) const;
+
+    /**
+     * @return the rotation flags of the current WireFrame
+     */
     int getRotation();
+
+    /**
+     * Sets the rotation of the current WireFrame
+     * @param axis new rotation flags
+     */
     void setRotation(int axis);
+
+    /**
+     * Toggles chosen rotations
+     * @param axis chosen axis based on RotationFlags enum
+     */
     void toggleRotation(int axis);
+
+    /**
+     * Rotates the current WireFrame
+     */
     void rotate();
+
+    /**
+     * Moves the current WireFrame
+     * @param amount the distance to move in each direction
+     */
     void updateLocation(const coord &amount);
 };
 
